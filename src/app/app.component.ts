dd1de072b0721511700737f984c0bb36e4ad70ef
Astryda Malinowska <astamalinowska@outlook.com>
2023-03-17 20:53:41 +0100

    Added field and component for input a new injection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,4 +30,12 @@ export class AppComponent {
   {
     return this.list.items.filter(item => !item.complete)
   }
+
+  addItem(newItem: string)
+  {
+    if (newItem != "")
+    {
+      this.list.addItem(newItem)
+    }
+  }
 }
